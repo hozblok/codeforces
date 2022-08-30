6292f311a20d8164b0f26a753be3d149accfe74e
Ivan Ergunov <hozblok@gmail.com>
2022-08-31 00:55:00 +0300

    1722 fix

--- 1722/G_Even_Odd_XOR.py
+++ 1722/G_Even_Odd_XOR.py
@@ -1,4 +1,3 @@
-import enum
 from functools import reduce
 import operator
 
@@ -11,66 +10,50 @@ for _ in range(tc):
     if n == 3:
         print("2 1 3")
         continue
-    a = []
-    b = []
-    val = 1
+    a = set()
+    b = set()
+    val = 2
     for i in range(n // 2):
-        a.append(val)
+        a.add(val)
         val += 1
-        b.append(val)
+        b.add(val)
         val += 1
     if n % 2 == 0:
         b.pop()
-    a_xor = reduce(operator.xor, a)
-    b_xor = reduce(operator.xor, b)
-    ax = f"{a_xor:031b}"
-    bx = f"{b_xor:031b}"
-    last = []
-    for i, el in enumerate(ax):
-        bel = bx[i]
-        if el == "0":
-            last.append("1" if bel == "1" else "0")
+    while True:
+        a_xor = reduce(operator.xor, a)
+        b_xor = reduce(operator.xor, b)
+        ax = f"{a_xor:031b}"
+        bx = f"{b_xor:031b}"
+        last = []
+        for i, el in enumerate(ax):
+            bel = bx[i]
+            if el == "0":
+                last.append("1" if bel == "1" else "0")
+            else:
+                last.append("1" if bel == "0" else "0")
+        res_last = int("".join(last), 2)
+        # print("".join(last), ax, bx, f"{(res_last):031b}")
+        if (res_last in a) or (res_last in b):
+            lst_b = b.pop()
+            lst_b += 1
+            while (lst_b in a) or (lst_b in b):
+                lst_b += 1
+            b.add(lst_b)
+            continue
+        if n % 2 == 0:
+            b.add(res_last)
         else:
-            last.append("1" if bel == "0" else "0")
-    res_last = int("".join(last))
-    if n % 2 == 0:
-        b.append(res_last)
-    else:
-        a, b = b, a
-        a.append(res_last)
+            a, b = b, a
+            a.add(res_last)
+        break
     # print(f"{n=}")
     # print(f"{a=} {b=}")
-    # print(a_xor, b_xor ^ res_last)
-    while a and b:
-        print(a.pop(), end=" ")
-        print(b.pop(), end=" ")
+    # print("@", a_xor, b_xor ^ res_last)
+    # print("@", reduce(operator.xor, a), reduce(operator.xor, b))
+    while a or b:
+        if a:
+            print(a.pop(), end=" ")
+        if b:
+            print(b.pop(), end=" ")
     print()
-
-    # res = [0] * n
-    # val = 1
-    # prev1 = None
-    # a1, a2 = 0, 0
-    # for i in range(0, n, 2):
-    #     res[i] = val
-    #     prev1 = a1
-    #     a1 ^= val
-    #     val += 1
-    # prev2 = None
-    # for i in range(1, n, 2):
-    #     res[i] = val
-    #     prev2 = a2
-    #     a2 ^= val
-    #     val += 1
-    # a = None
-    # prev = None
-    # if n % 2 == 1:
-    #     a = f"{a1:b}"
-    #     prev = f"{prev2:b}"
-    # else:
-    #     a = f"{a2:b}"
-    # a = "0" * (31 - len(a)) + a
-    # ares = f"{prev:b}"
-    # print(" ".join(map(str, res)))
-    # print(a1, a2)
-    # print(f"{prev1:b}", f"{prev2:b}")
-    # print()
